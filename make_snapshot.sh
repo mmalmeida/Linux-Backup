d4d00811ebe19fd0138af993f0f306a5f457cf6e
Miguel Almeida <miguel@almeida.at>
2012-01-06 16:09:06 +0000

    Tried around with sshfs on the script, reverted back to nfs

--- make_snapshot.sh
+++ make_snapshot.sh
@@ -11,9 +11,12 @@ unset PATH	# suggestion from H. Milz: avoid accidental use of $PATH
 # ------------- system commands used by this script --------------------
 ID=/usr/bin/id;
 ECHO=/bin/echo;
+DATE=/bin/date;
+PRINTF=/usr/bin/printf;
 
 CHMOD=/bin/chmod;
 MOUNT=/bin/mount;
+#MOUNT=/usr/bin/sshfs;
 UMOUNT=/bin/umount;
 RM=/bin/rm;
 MV=/bin/mv;
@@ -26,8 +29,8 @@ RSYNC=/usr/bin/rsync;
 # ------------- file locations -----------------------------------------
 
 #Machines needs to be on hosts file of the host
-MACHINES=(webservices)
-CONFIG_LOCATION=/home/malmeida/git/Linux-Backup/
+MACHINES=($HOSTNAME)
+CONFIG_LOCATION=/etc/backup/
 SNAPSHOT_RW=/mnt/machine
 BASE_LOCATION=/mnt/machine/
 MOUNT_DEVICE=192.168.10.100:/mnt/hdc/snapshots/
@@ -40,11 +43,13 @@ snapshot_machine(){
 	if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit; } fi
 
 	# attempt to remount the RW mount point as RW; else abort
-	## XXX - No nosso caso, sera um chmod provavel...
-	#$MOUNT -o remount,rw $MOUNT_DEVICE $SNAPSHOT_RW ;
-
 	if [ "$MACHINE" != "localhost" ] ; then
-#		$MOUNT -t nfs -o ro $MOUNT_DEVICE $SNAPSHOT_RW
+		$PRINTF '%s: Mounting device rw \n' "$($DATE '+%Y-%m-%d %H:%M')"
+#		if (!($MOUNT backup@$MOUNT_DEVICE $SNAPSHOT_RW -C -o nonempty)); then
+#			$ECHO "snapshot: could not mount $MOUNT_DEVICE";
+#	        	exit 1
+#		fi
+
 		$MOUNT -o remount,rw,nfsvers=3 $MOUNT_DEVICE $SNAPSHOT_RW ;
 		if (( $? )); then
 		{
@@ -52,10 +57,8 @@ snapshot_machine(){
 			exit;
 		}
 		fi;
-	else
-		BASE_LOCATION=/;
 	fi;
-
+	$PRINTF '%s: Device  mounted, moving snapshots \n' "$($DATE '+%Y-%m-%d %H:%M')"
 
 	# rotating snapshots of $MACHINE (fixme: this should be more general)
 
@@ -63,30 +66,39 @@ snapshot_machine(){
 	if [ -d $SNAPSHOT_RW/$MACHINE/daily.3 ] ; then			\
 	$RM -rf $SNAPSHOT_RW/$MACHINE/daily.3 ;				\
 	fi ;
+	$PRINTF '%s: Deleted daily.3 \n' "$($DATE '+%Y-%m-%d %H:%M')"
 
 	# step 2: shift the middle snapshots(s) back by one, if they exist
 	if [ -d $SNAPSHOT_RW/$MACHINE/daily.2 ] ; then			\
 	$MV $SNAPSHOT_RW/$MACHINE/daily.2 $SNAPSHOT_RW/$MACHINE/daily.3 ;	\
 	fi;
+	$PRINTF '%s: Pushed daily.2 to daily.3 \n' "$($DATE '+%Y-%m-%d %H:%M')"
+
 	if [ -d $SNAPSHOT_RW/$MACHINE/daily.1 ] ; then			\
 	$MV $SNAPSHOT_RW/$MACHINE/daily.1 $SNAPSHOT_RW/$MACHINE/daily.2 ;	\
 	fi;
+	$PRINTF '%s: Pushed daily.1 to daily.2 \n' "$($DATE '+%Y-%m-%d %H:%M')"
 
 	# step 3: make a hard-link-only (except for dirs) copy of the latest snapshot,
 	# if that exists
 	if [ -d $SNAPSHOT_RW/$MACHINE/daily.0 ] ; then			\
 	$CP -al $SNAPSHOT_RW/$MACHINE/daily.0 $SNAPSHOT_RW/$MACHINE/daily.1 ;	\
 	fi;
+	$PRINTF '%s: Pushed daily.0 to daily.1 \n' "$($DATE '+%Y-%m-%d %H:%M')"
 
 	# step 4: rsync from the system into the latest snapshot (notice that
 	# rsync behaves like cp --remove-destination by default, so the destination
 	# is unlinked first.  If it were not so, this would copy over the other
 	# snapshot(s) too!
+	$PRINTF '%s: Preparing to rsync... \n' "$($DATE '+%Y-%m-%d %H:%M')"
+
 	$RSYNC								\
-		-ave ssh --delete --delete-excluded				\
+		-ae ssh --delete --delete-excluded				\
 		--exclude-from="$EXCLUDES"				\
 		--include-from="$INCLUDES"				\
 		/ $SNAPSHOT_RW/$MACHINE/daily.0/ ;
+
+	$PRINTF '%s: Rsync finished! \n' "$($DATE '+%Y-%m-%d %H:%M')"
 
 	# step 5: update the mtime of daily.0 to reflect the snapshot time
 	$TOUCH $SNAPSHOT_RW/$MACHINE/daily.0 ;
@@ -94,20 +106,18 @@ snapshot_machine(){
 	# and thats it for $MACHINE.
 
 	# now remount the RW snapshot mountpoint as readonly
-	#$MOUNT -o remount,ro $MOUNT_DEVICE $SNAPSHOT_RW ;
 	if [ "$MACHINE" != "localhost" ] ; then
-#		$UMOUNT "$BASE_LOCATION";
 		$MOUNT -o remount,ro,nfsvers=3 $MOUNT_DEVICE $SNAPSHOT_RW ;
+#		fusermount -u $SNAPSHOT_RW;
 		if (( $? )); then
 		{
 			$ECHO "snapshot: could not unmount $BASE_LOCATION";
 			exit;
 		} fi;
-	fi;
+		$ECHO "Mounted device ro"
+		fi;
 }
 
-
-#$CHMOD -R 750 $SNAPSHOT_RW ;
 for i in ${MACHINES[@]}; do
 
 	MACHINE=${i};
@@ -115,4 +125,3 @@ for i in ${MACHINES[@]}; do
 	INCLUDES=$CONFIG_LOCATION$MACHINE"_include";
 	snapshot_machine;
 done
-#$CHMOD -R 550 $SNAPSHOT_RW ;
